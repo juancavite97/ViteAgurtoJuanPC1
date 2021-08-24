d3d830384f59fdaa6f5a14542c93b21553e8d2a4
Juan Vite <juan_vite@usmp.pe>
2021-08-24 18:44:43 -0500

    cambio final

---
+++ js/main.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+sr.reveal('.main', {
+    duration: 3000,
+    origin: 'left',
+    distance: '1000px'
+});
+
